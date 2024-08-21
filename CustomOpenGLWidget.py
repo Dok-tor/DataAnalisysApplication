Add external C++ library. Add more precise algorithm of log interpolation depends on angle between rotation matrix for low dim. Fixed sawing data.

CustomOpenGLWidget.py
@@ -47,7 +47,7 @@ class SplineType(Enum):
 
 
 class VisualizeDataWidget(QOpenGLWidget):
-    def __init__(self):
+    def __init__(self, outer_instance):
         super().__init__()
 
         self.aspect = self.width() / self.height()
@@ -65,6 +65,8 @@ class VisualizeDataWidget(QOpenGLWidget):
         self.line = None
         self.splineType = SplineType.ELLIPSE
         self.is_drawing_moving_now = False
+
+        self.outer_instance = outer_instance
 
     def setMode(self, mode):
         self.mode = mode
@@ -146,7 +148,7 @@ class VisualizeDataWidget(QOpenGLWidget):
 
         glBegin(GL_POINTS)
 
-        normalized_data = self.tour.getNormalizedData()
+        normalized_data = self.tour.getCurrentNormalizedData()
         rotated_points = np.dot(rotation_matrix, normalized_data.T).T
         for i, rotated_point in enumerate(rotated_points):
             # rotated_point = np.dot(rotation_matrix, point)
@@ -367,7 +369,7 @@ class VisualizeDataWidget(QOpenGLWidget):
         if self.line:
             highlighted_points = []
 
-            normalized_data = self.tour.getNormalizedData()
+            normalized_data = self.tour.getCurrentNormalizedData()
             rotation_matrix = self.rotation.as_matrix()
 
             rotated_points = np.dot(rotation_matrix, normalized_data.T).T
@@ -385,7 +387,7 @@ class VisualizeDataWidget(QOpenGLWidget):
 
             rotation_matrix = self.rotation.as_matrix()
 
-            rotated_points = np.dot(rotation_matrix, self.tour.getNormalizedData().T).T
+            rotated_points = np.dot(rotation_matrix, self.tour.getCurrentNormalizedData().T).T
             scaled_points = rotated_points[:, :2] * self.scale
 
             for i, scaled_point in enumerate(scaled_points):

DataSaver.py
@@ -20,6 +20,8 @@ def matrixToString(matrix: np.ndarray, labels_dict: dict = None) -> str:
                     # Подразумевается, что если словарь есть, значит последнее число в строке это метка класса
                     # Значит сделаем её целочисленной
                     string += str(int(matrix[i][j]))
+                else:
+                    string += str(matrix[i][j])  # Попытка сделать функцию универсальной, чтобы она печатала как обычные матрицы так и специальные
             else:
                 string += str(matrix[i][j])
                 string += '\t\t'
@@ -30,9 +32,16 @@ def matrixToString(matrix: np.ndarray, labels_dict: dict = None) -> str:
 
 
 class DataSaver:
-    def __init__(self, points: np.ndarray, labels: np.ndarray, labels_dict: dict = None) -> None:
+    def __init__(self, points: np.ndarray, labels: np.ndarray, original_dim: int, labels_dict: dict = None) -> None:
         self.points = points
         self.labels = labels
+        self.dim = original_dim
+        if self.dim == 2:
+            # Если оригинальная размерность была 2 значит мы добавили ещё один столбец, он мешает вычислению статистики
+            # следовательно его нужно убрать
+            self.points = points[:, :-1]
+        else:
+            self.points = points
         self.labels_dict = labels_dict
 
     def saveStatistics(self, path_to_stat_file: str) -> bool:

Ellipse.py
@@ -1,6 +1,7 @@
 import numpy as np
 from OpenGL.GL import *
 
+
 class Ellipse:
     def __init__(self, start_point, sensitivity=1.0):
         self.start_point = start_point
@@ -80,18 +81,6 @@ class Ellipse:
             self.center_y += y
 
     def isPointInside(self, x, y):
-        # ellipse_angle_rad = -np.radians(self.angle)
-        # cos_angle = np.cos(ellipse_angle_rad)
-        # sin_angle = np.sin(ellipse_angle_rad)
-        #
-        # # Матрица поворота для преобразования точек в систему координат эллипса
-        # cos_angle = np.cos(ellipse_angle_rad)
-        # sin_angle = np.sin(ellipse_angle_rad)
-        # ellipse_rotation_matrix = np.array([
-        #     [cos_angle, -sin_angle],
-        #     [sin_angle, cos_angle]
-        # ])
-
         translated_point = np.array([x - self.center_x, y - self.center_y])
         transformed_point = np.dot(self.ellipse_rotation_matrix, translated_point)
 